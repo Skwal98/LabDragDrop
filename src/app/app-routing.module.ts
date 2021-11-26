4e82d0caed793ef46e4b4aa501ddd829b979fb81
Skwal98 <levkalex98@gmail.com>
2021-11-26 22:32:00 +0300

    split to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DragComponent } from './drag/drag.component';
+import { DropComponent } from './drop/drop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'drag', component: DragComponent },
+  { path: 'drop', component: DropComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DropComponent } from './drop/drop.component';
+import { DragComponent } from './drag/drag.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DropComponent, DragComponent],
   imports: [BrowserModule, AppRoutingModule, DragDropModule],
   providers: [],
   bootstrap: [AppComponent],
